2710f2d2a575bbd84f2204590edc6a31606c3cf9
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-11 00:17:58 -0500

    feat: :lipstick: Se implemento un titulo que le de la bienvenida al cliente que ingrese a nuestra pagina
    Un saludo de bienvenida se implemento

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ class myframe extends HTMLElement{
     }
     connectedCallback(){
         this.shadowRoot.innerHTML = /*html*/`
-            <iframe class="spotify-iframe" width="350" height="500" src="https://open.spotify.com/embed/track/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <iframe class="spotify-iframe" width="350" height="370" src="https://open.spotify.com/embed/track/${this.id}" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
            
         `
         
