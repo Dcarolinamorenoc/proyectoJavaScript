776a15b7954275ae36da108a88a534c01f73792e
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-17 23:51:58 -0500

    feat: :construction: Creacion del input parte izquierda
    Se cre{o el input de la parte izquierda de la página

---
+++ js/modules/left.js
@@ -0,0 +1,55 @@
+
+
+class AlbumPictures extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async connectedCallback() {
+        const index = parseInt(this.getAttribute('index')) || 0;
+        this.loadAlbums('The weeknd', index);
+    }
+
+    async loadAlbums(searchTerm, index) {
+        const mainCode = searchTerm.replace(/\s/g, '%20');
+        const url = `https://spotify23.p.rapidapi.com/search/?q=${mainCode}&type=albums&offset=0&limit=10&numberOfTopResults=5`;
+        const options = {
+            method: 'GET',
+            headers: {
+                // 'X-RapidAPI-Key': 'a705b5fee9msh5b1aee0f41387b8p1de4e6jsn8c62c6ed641a',
+                // 'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.json();
+
+            if (result.albums.items.length > index) {
+                const albumData = result.albums.items[index].data;
+                if (albumData && albumData.coverArt && albumData.coverArt.sources.length > 0) {
+                    const primeraUrl = albumData.coverArt.sources[0].url;
+                    const uri = albumData.uri;
+                    const id = uri.split(':')[2];
+                    this.innerHTML = `
+                        <img id="album__${index + 1}" src="${primeraUrl}" alt="" data-id="${id}">
+                    `;
+
+                    this.querySelector('img').addEventListener('click', () => {
+                        const myFrame = document.querySelector('my-frame');
+                        myFrame.setAttribute('uri', `spotify:album:${id}`);
+                        const AlbumTracksComponent = document.querySelector('.trackList');
+                        AlbumTracksComponent.setAttribute('uri', `spotify:album:${id}`);
+                    });
+                }
+            } else {
+                this.innerHTML = `<p>No results found</p>`;
+            }
+        } catch (error) {
+            console.error(error);
+            this.innerHTML = `<p>Error loading albums</p>`;
+        }
+    }
+}
+
+customElements.define('album-pictures', AlbumPictures);
