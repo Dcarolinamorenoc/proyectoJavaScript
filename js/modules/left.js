875aa912c7fe269c4fe24fd8f75cbf7f97c0e966
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-05-18 02:27:40 -0500

    feat: :lipstick: Creacion de estilos albums
    Se generaron especialmente estilos para el apartado de albumes de la parte izquierda

--- js/modules/left.js
+++ js/modules/left.js
@@ -16,8 +16,8 @@ class AlbumPictures extends HTMLElement {
         const options = {
             method: 'GET',
             headers: {
-                // 'X-RapidAPI-Key': 'c62248404amsh7c48cf70d45bb8ep1550b3jsn37fe5e39063b',
-                // 'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+                'X-RapidAPI-Key': 'c62248404amsh7c48cf70d45bb8ep1550b3jsn37fe5e39063b',
+                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
             }
         };
 
